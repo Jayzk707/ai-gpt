93887144dd99a9063caa03fdf9787017069a8640
2C3 <141559544+Jayzk707@users.noreply.github.com>
2025-02-27 12:49:59 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -250,8 +250,8 @@ export function SideBar(props: { className?: string }) {
       {...props}
     >
       <SideBarHeader
-        title="NextChat"
-        subTitle="Build your own AI assistant."
+        title="KK2C3AI聊天机器人"
+        subTitle="KK2C3 AI assistant."
         logo={<ChatGptIcon />}
         shouldNarrow={shouldNarrow}
       >
